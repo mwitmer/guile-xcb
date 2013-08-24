1183bbb4ebd5c1cd9eb6be7afb2fd27acd979598
Mark Witmer <mark.d.witmer@gmail.com>
2013-08-24 18:43:47 -0400

    - Refactor and carefully document event loop code. - 'abort' is only called when necessary, - Error handling simplified and verified

--- flow/event-loop.scm
+++ flow/event-loop.scm
@@ -1,3 +1,26 @@
+;; This file is part of Guile XCB.
+
+;;    Guile XCB is free software: you can redistribute it and/or modify
+;;    it under the terms of the GNU General Public License as published by
+;;    the Free Software Foundation, either version 3 of the License, or
+;;    (at your option) any later version.
+
+;;    Guile XCB is distributed in the hope that it will be useful,
+;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;    GNU General Public License for more details.
+
+;;    You should have received a copy of the GNU General Public License
+;;    along with Guile XCB.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; These procedures provide a basic framework for running an event
+;; loop. Once inside the dynamic extent of a call to 'do-event-loop',
+;; the program is effectively transformed into a cooperative
+;; multithreaded environment. 'solicit' and 'notify' are provided to
+;; switch control to and from an event dispatcher as needed.
+
 (define-module (flow event-loop)
   #:use-module (ice-9 curried-definitions)
   #:use-module (ice-9 receive)
@@ -6,79 +29,115 @@
   #:use-module (ice-9 q)
   #:export (solicit))
 
-(define-public (make-tag data) (list data))
+(define-public (make-tag data)
+  "Create a unique object based on DATA to use as a tag for calls to
+'solicit'/'notify'."
+  (list data))
 
-(define-public notify-param (make-parameter #f))
-(define-public solicit-param (make-parameter #f))
-(define-public post-param (make-parameter #f))
+(define notify-param (make-parameter #f))
+(define solicit-param (make-parameter #f))
+(define post-param (make-parameter #f))
 
-(define-public (notify tag val)
+(define-public (notify tag . vals)
+  "Notify the event loop of the presence of a values VALS associated
+with object TAG. If any continuation or procedure is pending on the
+event loop for TAG as a result from a call to 'solicit', it will be
+called once and removed from the event loop. Otherwise, VALS will be
+queued for use in future calls to 'solicit'."
   (if (notify-param)
-      ((notify-param) tag val)
+      (apply (notify-param) tag vals)
       (error "event-loop: Call to notify outside an event-loop")))
 
 (define* (solicit tag #:optional (proc *unspecified*))
+  "Place a procedure on the event loop to run when some call to
+'notify' provides a value for object TAG. If PROC is provided,
+'solicit' returns immediately with an unspecified value. Otherwise,
+control flow is yielded to the event loop until 'notify' is called for
+TAG, at which point this procedure returns the given value(s) and
+control flow returns to its continuation ."
   (if (solicit-param)
       ((solicit-param) tag proc)
       (error "event-loop: Call to solicit outside an event-loop")))
 
 (define-public (post-to-event-loop thunk)
+  "Queues THUNK to be executed the next time control is yielded to the
+event loop."
   (if (post-param)
       ((post-param) thunk)
       (error "event-loop: Call to post-to-event-loop outside an event-loop")))
 
-(define-public (unsolicit tag) (solicit tag #f))
+(define error-tag (make-tag 'error))
+(define tick-tag (make-tag 'tick))
 
-(define-public error-tag (make-tag 'error))
-(define-public tick-tag (make-tag 'tick))
+(define-public (event-loop-tick)
+  "Yields control flow to the event loop for only a single call to the
+event dispatcher. Returns the result of that call."
+  (solicit tick-tag))
 
-(define ((solicit-proc conts early post-q dispatcher) tag proc)
-  (shift cont ((core-loop conts early post-q dispatcher) cont tag proc)))
+(define-public (event-loop-error . args)
+  "Signals an error to be handled by the event loop's installed error
+handler."
+  (apply notify error-tag args))
 
-(define ((notify-proc conts early) tag val)
-  (if (eq? tag error-tag) (abort-to-prompt error-tag val))
-  (let ((cont (hashq-ref conts tag)))
-    (hashq-remove! conts tag)
-    (if cont (cont val)
-        (let ((earlies (or (hashq-ref early tag)
-                           (hashq-set! early tag (make-q)))))
-          (enq! earlies val)))))
-
-(define ((post-proc post-queue) thunk) (q-push! post-queue thunk))
-
-(define ((core-loop conts early post-q dispatcher) cont key proc)
-  (define (dispatch-loop)
-    (while (not (q-empty? post-q)) ((q-pop! post-q)))
-    (let ((result (dispatcher)))
-      (cond
-       ((eq? cont (hashq-ref conts key)) (dispatch-loop))
-       ((hashq-ref conts tick-tag) => (lambda (c) (c result)))
-       (else result))))
-  (define (on-miss)
-    (hashq-set! conts key (if (unspecified? proc) cont proc))
-    (if (unspecified? proc) (dispatch-loop) (cont #t)))
-  (define (on-hit early-vals)
-    (define val (deq! early-vals))
-    (if (q-empty? early-vals) (hashq-remove! early key))
-    ((if (unspecified? proc) cont proc) val))
-  (cond
-   ((not proc) (hashq-remove! conts key) (cont))
-   ((hashq-ref early key) => on-hit)
-   (else (on-miss))))
+(define-public (unsolicit tag)
+  "Removes the pending procedure or continuation on the event loop for
+TAG if one is present."
+  (solicit tag #f))
 
 (define-public (do-event-loop dispatcher proc on-error)
-  (define (run)
-    (let ((conts (make-weak-key-hash-table))
-          (early (make-weak-key-hash-table))
-          (post-q (make-q)))
-      (parameterize
-          ((notify-param (notify-proc conts early))
-           (solicit-param (solicit-proc conts early post-q dispatcher))
-           (post-param (post-proc post-q)))
-        (reset (proc)))))
-  (call-with-prompt error-tag run on-error))
+  "Runs PROC inside of an event loop. DISPATCHER is a non-blocking
+thunk that reads and handles events.
+
+See 'solicit' and 'notify' for an explanation on how to transfer
+control flow between PROC and DISPATCHER.
+
+ON-ERROR is a function that will be called when 'event-loop-error' is
+called. For its arguments, it takes RESUME, a function that returns to
+the event loop, and then all the values that were passed to
+'event-loop-error'."
+  (define conts (make-weak-key-hash-table))
+  (define early (make-weak-key-hash-table))
+  (define-syntax-rule (dispatch expr) (% expr run-dispatch))
+  (define-syntax-rule (error-prompt expr) (% error-tag expr handle-error))
+  (define post-q (make-q))
+  (define (notify-proc tag . vals)
+    (if (eq? tag error-tag) (apply abort-to-prompt error-tag vals)
+        (let ((cont (hashq-ref conts tag)))
+          (hashq-remove! conts tag)
+          (if cont (apply cont vals)
+              (let ((earlies (or (hashq-ref early tag)
+                                 (hashq-set! early tag (make-q)))))
+                (enq! earlies vals))))))
+  (define (solicit-proc key proc)
+    (define (on-miss)
+      (if (unspecified? proc) (abort key) (hashq-set! conts key proc)))
+    (define (on-hit early-vals)
+      (define vals (deq! early-vals))
+      (if (q-empty? early-vals) (hashq-remove! early key))
+      (if (unspecified? proc) (apply values vals) (apply proc vals)))
+    (cond
+     ((not proc) (hashq-remove! conts key))
+     ((hashq-ref early key) => on-hit)
+     (else (on-miss))))
+  (define (post-proc thunk) (q-push! post-q thunk))
+  (define (run-dispatch cont key)
+    (hashq-set! conts key cont)
+    (let get-result ((result (dispatch (dispatcher))))
+      (while (not (q-empty? post-q)) (dispatch ((q-pop! post-q))))
+      (cond
+       ((eq? key tick-tag) (dispatch (notify tick-tag result)))
+       ((eq? cont (hashq-ref conts key)) (get-result (dispatch (dispatcher))))
+       (else result))))
+  (define (handle-error cont . args)
+    (error-prompt (apply on-error cont args)))
+  (error-prompt (parameterize ((notify-param notify-proc)
+                               (solicit-param solicit-proc)
+                               (post-param post-proc))
+                  (dispatch (proc)))))
 
 (define-public (notify-map reply-tags)
+  "Solicits all tags in the list REPLY-TAGS and returns a single tag
+that will be notified when all of the other tags have been notified."
   (define not-ready (make-tag 'not-ready))
   (define replies
     (map (lambda (reply-tag) (cons reply-tag (make-parameter not-ready)))
--- xcb/event-loop.scm
+++ xcb/event-loop.scm
@@ -1,3 +1,25 @@
+;; This file is part of Guile XCB.
+
+;;    Guile XCB is free software: you can redistribute it and/or modify
+;;    it under the terms of the GNU General Public License as published by
+;;    the Free Software Foundation, either version 3 of the License, or
+;;    (at your option) any later version.
+
+;;    Guile XCB is distributed in the hope that it will be useful,
+;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;    GNU General Public License for more details.
+
+;;    You should have received a copy of the GNU General Public License
+;;    along with Guile XCB.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; These procedures provide an X-specific implementation of (flow
+;; event-loop), with helper macros and procedures intended to make it
+;; easy to receive replies to X requests, events, and errors both
+;; synchronously and asynchronously.
+
 (define-module (xcb event-loop)
   #:use-module (ice-9 curried-definitions)
   #:use-module (ice-9 receive)
@@ -32,16 +54,40 @@
    on-unknown-event (make-hash-table) (make-hash-table)
    basic-error-handler))
 
-(define (event-loop-prepared? xcb-conn)
+(define-public (event-loop-prepared? xcb-conn)
+  "-- Scheme Procedure: event-loop-prepared? xcb-conn
+     Returns `#t' if XCB-CONN is prepared for use in an event loop."
   (not (not (xcb-connection-data xcb-conn))))
 
 (define* (event-loop-prepare! xcb-conn #:optional error-handler)
+  "-- Scheme Procedure: event-loop-prepare! xcb-conn error-handler
+     Prepares XCB-CONN for use in an event loop. ERROR-HANDLER is a
+     procedure that will be called when the X server sends an error.
+     It takes two arguments: RESUME, a procedure that, when called,
+     will return control back to the event loop, and DATA, a vector
+     returned from `poll-xcb-connection'.
+
+     It is only necessary to call this procedure before calling
+     `xcb-event-loop' if ERROR-HANDLER is provided, or if reply/event
+     handler procedures are going to be attached to the connection
+     before the event loop starts. Otherwise, XCB-CONN will
+     automatically be prepared with the default error handler at that
+     point."
   (define event-loop-data (make-event-loop-data))
   (if error-handler (set-default-error-handler! event-loop-data error-handler))
   (set-xcb-connection-data! xcb-conn event-loop-data))
 
 (define-public listen-default!
   (case-lambda
+    "-- Scheme Procedure: listen-default! [xcb-conn] proc
+     Installs PROC as the default procedure to call when an event is
+     not otherwise handled by XCB-CONN.
+
+     PROC will be called with the event as its argument; if the event
+     is not recognized by the connection, perhaps because it pertains
+     to an extension that has not been enabled, a bytevector
+     containing the raw binary data from the server will be passed
+     instead."
     ((xcb-conn proc)
      (set-event-default! (xcb-connection-data xcb-conn) proc))
     ((proc)
@@ -49,6 +95,8 @@
 
 (define-public unlisten-default!
   (case-lambda
+    "-- Scheme Procedure: unlisten-default! [xcb-conn]
+     Removes the default event handler from XCB-CONN."
     ((xcb-conn)
      (set-event-default!
       (xcb-connection-data xcb-conn)
@@ -68,6 +116,21 @@
 
 (define-public listen!
   (case-lambda
+    "-- Scheme Procedure: listen! [xcb-conn] event-type tag proc [guard]
+     Add procedure PROC to the list of procedures that may be called
+     when an event of type EVENT-TYPE is received by XCB-CONN. PROC
+     takes one argument, EVENT, which is an XCB structure
+     corresponding to the event from the server.
+
+     TAG is any value that can uniquely identify the procedure,
+     making it easier to later remove the procedure from the list. Any
+     procedure already associated with TAG for the given EVENT-TYPE
+     will be overridden by the new one.
+
+     GUARD, if present, should be a predicate that takes the same
+     argument as PROC and returns `#t' if PROC should be called, or
+     `#f' if the event does not match some set of criteria and PROC
+     should be skipped."
     ((a b c d)
      (define (listen-with-connection xcb-conn struct tag proc)
        (inner-listen! xcb-conn struct tag proc))
@@ -92,6 +155,10 @@
 
 (define-public unlisten!
   (case-lambda
+    "-- Scheme Procedure: unlisten! [xcb-conn] event-type tag
+     Removes the event handler procedure associated with value TAG
+     from the list of event handlers on event type EVENT-TYPE for xcb
+     connection XCB-CONN."
     ((a b)
      (define (unlisten-with-connection xcb-conn struct)
        (unlisten-inner! xcb-conn struct))
@@ -117,6 +184,11 @@
 
 (define-public reply-listen!
   (case-lambda
+    "-- Scheme Procedure: reply-listen! [xcb-conn] sequence-number
+          reply-proc
+     When a reply with SEQUENCE-NUMBER is received by XCB-CONN, calls
+     REPLY-PROC with the reply as an argument. The handler is removed
+     after it has been called."
     ((xcb-conn sequence-number reply-proc)
      (hashv-set!
       (reply-handlers (xcb-connection-data xcb-conn))
@@ -127,6 +199,11 @@
       sequence-number reply-proc))))
 
 (define-public (xcb-event-loop xcb-conn proc)
+  "-- Scheme Procedure: xcb-event-loop xcb-conn proc
+     Execute thunk PROC in a dynamic scope where
+     `current-xcb-connection' is bound to XCB-CONN, and an event loop
+     for handling calls to the `solicit'/`notify' procedures is in
+     place."
   (define loop-data
     (begin
       (if (not (event-loop-prepared? xcb-conn)) (event-loop-prepare! xcb-conn))
@@ -149,6 +226,7 @@
                  default))
             ((reply)
              (and=> (hashv-remove! replies (xcb-sequence-number data)) cdr))
+            ((error) (event-loop-error data) #f)
             (else #f)))
         (if (and dispatch-proc data) (dispatch-proc (xcb-data data)) #f))
       (call-with-values poll dispatch))
@@ -160,6 +238,13 @@
     (do-event-loop dispatch proc on-error)))
 
 (define-public (delay-reply proc . args)
+  "-- Scheme Procedure: delay-reply proc . args
+     Call XCB request procedure PROC on the current xcb connection
+     with arguments ARGS. Returns a tag that, when used in a call to
+     `solicit', will yield the corresponding reply for the request. Do
+     not use this procedure for a request that does not receive a
+     reply from the server, or else the procedure that called it will
+     never resume."
   (define notify-tag (make-tag `(xcb-cookie ,proc)))
   (define value (make-parameter #f))
   (reply-listen!
@@ -167,15 +252,28 @@
    (lambda (reply) (notify notify-tag reply)))
   notify-tag)
 
-(define-public (reply-for proc . args) (solicit (apply delay-reply proc args)))
+(define-public (reply-for proc . args) 
+  "-- Scheme Procedure: reply-for proc . args
+     Calls `delay-reply' on the given arguments and then immediately
+     solicits and returns the reply."
+  (solicit (apply delay-reply proc args)))
 
 (define-syntax-rule (with-connection xcb-conn stmt ...)
+  "-- Scheme Syntax: with-connection xcb-conn stmt ...
+     Creates a thunk with body STMT ... and runs an event loop until
+     the thunk is completed."
   (xcb-event-loop xcb-conn (lambda () stmt ...)))
 
 (define-syntax-rule (loop-with-connection xcb-conn stmt ...)
-  (xcb-event-loop xcb-conn (lambda () stmt ...
-                                   (while (xcb-connected? xcb-conn)
-                                     (solicit tick-tag)))))
+  "-- Scheme Syntax: loop-with-connection xcb-conn stmt ...
+     Creates a thunk with body STMT ... and runs an event loop until
+     the thunk is completed and the xcb connection is also
+     disconnected."
+  (xcb-event-loop 
+   xcb-conn
+   (lambda () 
+     stmt ... (while (xcb-connected? (current-xcb-connection)) 
+                (event-loop-tick)))))
 
 (define (verify-fields event . plist)
   (let verify ((plist plist))
@@ -184,7 +282,7 @@
               (val2 (cadr plist)))
           (if (xcb= val1 val2) (verify (cddr plist)) #f)))))
 
-(define-syntax make-listener
+(define-syntax make-listener  
   (syntax-rules ()
     ((_ event-struct tag name (guard guard* ...) expr expr* ...)
      (listen! event-struct tag
@@ -193,8 +291,29 @@
     ((_ event-struct tag name () expr expr* ...)
      (listen! event-struct tag (lambda (name) expr expr* ...)))))
 
-(define-syntax create-listener
+(define-syntax create-listener  
   (syntax-rules ()
+    "-- Scheme Syntax: create-listener (stop! reset! reset-expr ...)
+          ((event-struct event #:field value ...) body body* ...) ...
+     Adds a set of event listeners to the current xcb connection. Each
+     EVENT-STRUCT is an XCB struct type for an event and EVENT is
+     bound to the event within the accompanying expressions BODY
+     BODY* ....
+
+     #:FIELD is a keyword for a field of the event struct (eg.
+     #:window, #:event, #:count, etc.), and VALUE is the required
+     value for that field. There can be any number of #:FIELD/VALUE
+     pairs. The body expressions will only be evaluted if the fields
+     of the event match the required values.
+
+     STOP! is the name for a procedure that can be called anywhere in
+     a body expression that removes the listeners from the connection.
+
+     RESET! is the name for a procedure that executes the expressions
+     RESET-EXPR ... and then reinstates the listeners. It is called
+     once to start the listeners when the macro is first evaluated.
+
+     The macro returns STOP! and RESET! as two values."
     ((_ (stop! reset! reset-expr ...)
         ((event-struct name guard ...) body body* ...) ...)
      (create-tagged-listener (make-tag 'listener) (stop! reset! reset-expr ...)
@@ -203,11 +322,18 @@
      (create-listener
          (stop! reset!) ((event-struct name guard ...) body body* ...) ...))
     ((_ () ((event-struct name guard ...) body body* ...) ...)
-     (create-listenern
+     (create-listener
          (stop! reset!) ((event-struct name guard ...) body body* ...) ...))))
 
-(define-syntax create-tagged-listener
+(define-syntax create-tagged-listener  
   (syntax-rules ()
+    "-- Scheme Syntax: create-listener tag ...
+     Identitical to `create-tagged-listener', but requires an
+     additional argument TAG, which will is used as the tag argument
+     in `listen!'/`unlisten!'.
+
+     Use `create-tagged-listener' if newly created listeners are meant
+     to replace pre-existing ones with the same tag."
     ((_ tag (stop! reset! reset-expr ...)
         ((event-struct name guard ...) body body* ...) ...)
      (letrec* ((stop! (lambda () (unlisten! event-struct tag) ...))
@@ -235,6 +361,15 @@
     notify-tag))
 
 (define-syntax-rule (with-replies ((reply proc arg ...) ...) stmt stmt* ...)
+  "-- Scheme Syntax: with-replies ((reply proc arg ...) ...) stmt stmt*
+          ...
+     Calls each XCB request procedure PROC with its associated ARG
+     list as arguments. When all of the replies are received, they
+     are each bound to the corresponding symbol REPLY and used as
+     arguments to call a procedure with body STMT STMT* ... .
+
+     Returns a tag that can be used in a call to `solicit' to wait for
+     the procedure to execute and then return its result."
   (let* ((pre-notify-tag (notify-map (list (delay-reply proc arg ...) ...)))
          (notify-tag (make-tag '(with-replies (reply <- proc) ...)))
          (on-complete (lambda (reply ...) stmt stmt* ...))
@@ -245,6 +380,11 @@
     notify-tag))
 
 (define-public (delay-enable-extension extension-key)
+  "-- Scheme Procedure: delay-enable-extension extension-key
+     Sends a `query-extension' request to the server and, if the
+     extension is present, enables that extension for the current xcb
+     connection when the reply arrives. Returns a tag for use with
+     `solicit' to defer to the event loop until the reply arrives."
   (define extension-info (get-extension-info extension-key))
   (with-replies ((query query-extension (car extension-info)))
     ((cdr extension-info) query)))
